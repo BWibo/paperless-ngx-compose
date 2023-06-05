e2c3dbb6e1479fc49df851e891ecce019e231968
Bruno Willenborg <b.willenborg@tum.de>
2023-06-05 09:23:22 +0200

    Add backup script

---
+++ backup/paperless-backup-restic.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# Crontab entry ---------------------------------------------------------------
+# 0 4 * * * /path/to/paperless-backup-restic.sh >/dev/null 2>&1
+
+# Note: Make sure export folder exists and restic repos are initialized!
+
+# config ----------------------------------------------------------------------
+PAPERLESS_BACKUP_PATHS="/media/paperless/export/backup /media/paperless/consume"
+LOGFILE="/media/paperless/export/backup.log"
+ERR=0
+
+# Restic
+export RESTIC_PASSWORD="${PAPERLESS_RESTIC_PASSWORD:-changeMe}"
+
+# Azure
+export AZURE_ACCOUNT_NAME="${PAPERLESS_AZURE_ACCOUNT_NAME:-accountname}"
+export AZURE_ACCOUNT_KEY="${PAPERLESS_AZURE_ACCOUNT_KEY:-changeMe}"
+
+# script ----------------------------------------------------------------------
+printf "\n\n" >> ${LOGFILE}
+echo "-- Paperless backup " `date --utc +%FT%TZ` "-----------------------------" >> ${LOGFILE} 2>&1
+
+# Create paperless export
+  restic backup --no-scan >> ${LOGFILE} 2>&1
+docker exec -i -t paperless-webserver-1 document_exporter ../export/backup >> ${LOGFILE} 2>&1
+
+errtmp=$?
+ERR=$(($ERR + $errtmp))
+echo "create paperless export" $errtmp >> ${LOGFILE}
+
+# Create testic snapshot - local
+export RESTIC_REPOSITORY=/home/bruno/restic/paperless
+echo "$BACKUPDIR_DB_TEMP $PAPERLESS_BACKUP_PATHS" | \
+  xargs \
+  restic backup --no-scan >> ${LOGFILE} 2>&1
+
+errtmp=$?
+ERR=$(($ERR + $errtmp))
+echo "create restic snapshot - local" $errtmp >> ${LOGFILE} 2>&1
+
+# Create restic snapshot - Azure
+export RESTIC_REPOSITORY="azure:restic:/paperless"
+echo "$BACKUPDIR_DB_TEMP $PAPERLESS_BACKUP_PATHS" | \
+  xargs \
+  restic backup --no-scan >> ${LOGFILE} 2>&1
+
+errtmp=$?
+ERR=$(($ERR + $errtmp))
+echo "create restic snapshot - Azure" $errtmp >> ${LOGFILE}
+
+# Cleanup paperless export
+rm -rf /media/paperless/export/backup/*
+errtmp=$?
+ERR=$(($ERR + $errtmp))
+echo "cleanup paperless export" $errtmp >> ${LOGFILE}
+
+# Cleanup restic repos
+# local
+export RESTIC_REPOSITORY=/home/bruno/restic/paperless
+restic forget --prune --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 5 >> ${LOGFILE} 2>&1
+
+errtmp=$?
+ERR=$(($ERR + $errtmp))
+echo "cleanup restic snapshots - local " $errtmp >> ${LOGFILE}
+
+# Azure
+export RESTIC_REPOSITORY="azure:restic:/paperless"
+restic forget --prune --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 2 >> ${LOGFILE} 2>&1
+
+errtmp=$?
+ERR=$(($ERR + $errtmp))
+echo "cleanup restic snapshots - Azure " $errtmp >> ${LOGFILE}
+
+echo "Total ERR" $ERR >> ${LOGFILE}
